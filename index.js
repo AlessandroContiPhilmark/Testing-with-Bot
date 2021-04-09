b91bc9edc12b2d34cdc07c337cb2de4166ec3d80
alecontisb <52315312+alecontisb@users.noreply.github.com>
2021-04-09 15:12:55 +0200

    update

--- index.js
+++ index.js
@@ -55,84 +55,85 @@ async function login(page_1){
     await page_1.evaluate(() => showPlayerCorso())
     await timer(3 * 1000)
 }
-(async () => {
-    var browser = await puppeteer.launch(puppeteerConfig).catch( error => {
-        console.error('Could not launch browser');
-        throw error;
-    });
-    var page_1 = (await browser.pages())[0];
-
-    module.exports = { browser, page_1 };
-    page_1.on('pageerror', (err) => {
-        console.error(err);
-    });
-
-    function dialog(dialog) {
-        dialogPromise = new Promise(async res => {
-            console.log("Dialog message: " + dialog.message())
-            if(dialog.message() === "Account in uso. Disconnettere l'altro utente?"){
-                await dialog.accept()
-                await page_1.type('#sicuruser', USERNAME)
-                await page_1.type('#sicurpass', PASSWORD)
-                page_1.click('button.sicurweb-login-submit')
-                await page_1.waitForNavigation(navigationOptions)
-            }
-            if(dialog.message().includes("Vuoi riprendere dall'ultima slide")){
-                await dialog.accept()
-            }
-            res();
-        })
-    }
-    page_1.on('dialog', dialog); 
-
-    await page_1.setViewport(viewport),
-    await login(page_1)
-
-
-
-    //closes all open folders!
-    await page_1.evaluate(() => $('#ygtvc9 > .ygtvitem').find('.ygtvtm').click())
-    //Open current folder
-    await page_1.evaluate(folderIndex => $('#ygtvc9 > .ygtvitem').find('.ygtvtp, .ygtvlp')
-        .get(folderIndex).click()
-    , folderIndex)
-    await timer(2 * 1000)
-
-
-    var slide = await page_1.$$('#ygtvc9 > .ygtvitem > .ygtvchildren > .ygtvitem .ygtvcontent')
-    slide = slide[slideIndex]
-    var slideName = await slide.$$eval('td', elem => elem[1].textContent)
-    await slide.click()
-    await page_1.waitForSelector('.ps-current ul li img')
-    var images = await page_1.evaluate(() => $('.ps-current ul li img').map(function(){
-        return $(this).attr('src')
-    }).toArray())
-
-    var mainFolder = "./pdfs"
-    if (!fs.existsSync(mainFolder))
-        fs.mkdirSync(mainFolder);
-    var folderPath = mainFolder + "/" + slideName
-    if (!fs.existsSync(folderPath))
-        fs.mkdirSync(folderPath)
-    if(createImages){
-        var page_2 = await browser.newPage()
-        await page_2.setViewport(viewport)
-        await images.asyncForEach(async (imgUrl, index) => {
-            var path = folderPath + "/" + slideName + "_" + (++index) + ".png"
-            if(overrideImages || !fs.existsSync(path)){
-                var url = domain + 'fad/' + imgUrl
-                await page_2.goto(url, navigationOptions)
-                console.log('Navigated to: ' + url)
-                await page_2.screenshot({ path })
-                console.log('Created: ' + path)
-            } else console.log('Already existing: ' + path)
-        })
-        await page_2.close()
-    }
-    await timer(3 * 1000)
-    var endCycle = false
-    while(!endCycle){
-        try {
+async function play(){
+    var browser
+    try {
+        browser = await puppeteer.launch(puppeteerConfig).catch( error => {
+            console.error('Could not launch browser');
+            throw error;
+        });
+        var page_1 = (await browser.pages())[0];
+    
+        module.exports = { browser, page_1 };
+        page_1.on('pageerror', (err) => {
+            console.error(err);
+        });
+    
+        function dialog(dialog) {
+            dialogPromise = new Promise(async res => {
+                console.log("Dialog message: " + dialog.message())
+                if(dialog.message() === "Account in uso. Disconnettere l'altro utente?"){
+                    await dialog.accept()
+                    await page_1.type('#sicuruser', USERNAME)
+                    await page_1.type('#sicurpass', PASSWORD)
+                    page_1.click('button.sicurweb-login-submit')
+                    await page_1.waitForNavigation(navigationOptions)
+                }
+                if(dialog.message().includes("Vuoi riprendere dall'ultima slide")){
+                    await dialog.accept()
+                }
+                res();
+            })
+        }
+        page_1.on('dialog', dialog); 
+    
+        await page_1.setViewport(viewport),
+        await login(page_1)
+    
+    
+    
+        //closes all open folders!
+        await page_1.evaluate(() => $('#ygtvc9 > .ygtvitem').find('.ygtvtm').click())
+        //Open current folder
+        await page_1.evaluate(folderIndex => $('#ygtvc9 > .ygtvitem').find('.ygtvtp, .ygtvlp')
+            .get(folderIndex).click()
+        , folderIndex)
+        await timer(2 * 1000)
+    
+    
+        var slide = await page_1.$$('#ygtvc9 > .ygtvitem > .ygtvchildren > .ygtvitem .ygtvcontent')
+        slide = slide[slideIndex]
+        var slideName = await slide.$$eval('td', elem => elem[1].textContent)
+        await slide.click()
+        await page_1.waitForSelector('.ps-current ul li img')
+        var images = await page_1.evaluate(() => $('.ps-current ul li img').map(function(){
+            return $(this).attr('src')
+        }).toArray())
+    
+        var mainFolder = "./pdfs"
+        if (!fs.existsSync(mainFolder))
+            fs.mkdirSync(mainFolder);
+        var folderPath = mainFolder + "/" + slideName
+        if (!fs.existsSync(folderPath))
+            fs.mkdirSync(folderPath)
+        if(createImages){
+            var page_2 = await browser.newPage()
+            await page_2.setViewport(viewport)
+            await images.asyncForEach(async (imgUrl, index) => {
+                var path = folderPath + "/" + slideName + "_" + (++index) + ".png"
+                if(overrideImages || !fs.existsSync(path)){
+                    var url = domain + 'fad/' + imgUrl
+                    await page_2.goto(url, navigationOptions)
+                    console.log('Navigated to: ' + url)
+                    await page_2.screenshot({ path })
+                    console.log('Created: ' + path)
+                } else console.log('Already existing: ' + path)
+            })
+            await page_2.close()
+        }
+        await timer(3 * 1000)
+        var endCycle = false
+        while(!endCycle){
             await page_1.waitForSelector('.minutes')
             await page_1.waitForSelector('.seconds')
             await page_1.waitForSelector('.pgwSlideshow .ps-caption b')
@@ -152,11 +153,15 @@ async function login(page_1){
             )
             await page_1.$eval('.ps-next', elem => elem.click())
             await timer(randomTimer * 60  * 1000)
-        } catch (error) {
-            console.error(error)
         }
+    } catch (error) {
+        console.error(error)
+        try { await browser.close(); } catch (error) {
+            console.log('Error while closing browser ' + error)
+        }
+        console.log('Restarting in 5 seconds')
+        await timer(5 * 1000)
+        play()
     }
-    console.log('Urls finished, closing browser');
-    await page_1.close();
-    await browser.close();
-})()
+}
+play()
